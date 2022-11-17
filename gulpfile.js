714f29dcd1640892541b592d3b1ae2192743a682
sexfat <bryant6831@gmail.com>
2022-11-17 14:05:02 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -7,61 +7,13 @@ const {
 } = require('gulp');
 
 
-function defaultTask(cb) {
-    console.log('gulp 成功');
-    cb();
-}
-
-exports.c = defaultTask;
-
-
-
-// taskA
-function A(cb) {
-    console.log('a mission');
-    cb();
-}
-
-//taskB 
-function B(cb) {
-    console.log('b mission');
-    cb();
-}
-
-
-exports.sync = series(A, B);
-exports.async = parallel(A, B);
-
-// 以上使用在最後打包的流程控管
-
-
-function file() {
-    return src(['src/*.html', 'src/*.css', 'src/**/*.js', '!src/about.html'])
-        .pipe(dest('dist/')) // 打包兩種不同檔案格式  !排除  **下一層目錄
-}
-
-exports.f = file;
-
-
-
-
 
 const rename = require('gulp-rename');
 
 //壓縮css
 const cleanCSS = require('gulp-clean-css');
 
-function cssminify() {
-    return src('src/*.css')
-        .pipe(cleanCSS())
-        .pipe(rename({
-            extname: ".min.css"
-        }))
-        .pipe(dest('dist/css'))
-}
 
-
-exports.css = cssminify
 
 
 //  壓縮 js
@@ -71,33 +23,11 @@ const uglify = require('gulp-uglify');
 function js() {
     return src('src/js/*.js')
         .pipe(uglify())
-        .pipe(rename({
-            extname: ".min.js"
-        }))
         .pipe(dest('dist/js'));
 }
 
-exports.minijs = js;
-
-//====== 同時壓縮 css js ======
-
-exports.combine = parallel(js, cssminify)
-
-// ============ end ============
 
 
-// rename 改檔名
-
-
-function cssname() {
-    return src('src/*.css')
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(dest('dist/css'))
-}
-
-exports.re = cssname;
 
 
 //sass => css
@@ -122,7 +52,6 @@ exports.style = styleSass;
 // html layout
 const fileinclude = require('gulp-file-include');
 
-
 function html() {
     return src('src/*.html')
         .pipe(fileinclude({
@@ -133,21 +62,8 @@ function html() {
 }
 
 
-exports.h = html;
+ //exports.h = html;
 
-
-function watchfile(){
-   watch(['src/*.html' , 'src/layout/*.html'], html)
-   watch(['src/sass/*.scss' , 'src/sass/**/*.scss'] , styleSass)
-}
-
-
-exports.w = watchfile;
-
-
-
-
-// ======== 上線用 =======
 
 //  7 壓縮圖片
 const imagemin = require('gulp-imagemin');
@@ -204,7 +120,7 @@ function browser(done) {
         },
         port: 3000
     });
-    watch(['src/*.html' , 'src/layout/*.html'], html).on('change' , reload)
+    watch(['src/*.html' , 'src/layout/*.html' , 'src/layout/**/*.html'], html).on('change' , reload)
     watch(['src/sass/*.scss' , 'src/sass/**/*.scss'] , styleSass).on('change' , reload)
     watch(['src/images/*.*' , 'src/images/**/*.*'] , img).on('change' , reload)
     watch('src/js/*.js' , js).on('change' , reload)
