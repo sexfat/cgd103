fd48940f858383b543c03f81b4b948d1a26e5d1d
sexfat <bryant6831@gmail.com>
2022-11-25 10:52:18 +0800

    add style loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import gsap from "gsap";
-
+import './css/style.css'; //css注入
 
 console.log('start');
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,18 @@ module.exports = {
        path: path.resolve(__dirname, 'dist'),
        filename : 'app.js'
     },              // 出口文件
-    // module: {},              // 處裡對應模組
+    module: {
+        rules: [{
+            // 格式
+            test: /\.css$/,
+            //順序是由下到上 css > style
+            use: [
+                'style-loader',
+                'css-loader'
+            ],
+        }]
+
+    },               // 處裡對應模組
     // plugins: [],             // 對應的插件
     // devServer: {},           // 服務器配置
     mode: 'development'      // 開發模式配置 production | development 
