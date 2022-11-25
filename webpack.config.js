6492d3ca861e84d0c79f66aa7b414304a3fd9d3f
sexfat <bryant6831@gmail.com>
2022-11-25 11:34:21 +0800

    add html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',               // 入口文件
+    entry: {
+         app :'./src/index.js'  // app => chunks  
+    },               // 入口文件
     output: {
-       path: path.resolve(__dirname, 'dist'),
-       filename : 'app.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
     },              // 出口文件
     module: {
         rules: [{
@@ -15,9 +18,9 @@ module.exports = {
             use: [{
                 loader: MiniCssExtractPlugin.loader,
                 options: {
-                  publicPath: './dist'
+                    publicPath: './dist'
                 }
-              },
+            },
                 'css-loader',
                 'sass-loader'
             ],
@@ -26,9 +29,17 @@ module.exports = {
     },          // 處裡對應模組
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "./style.css"
+            filename: "./[name].css"
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['app'],  //選擇注入資源 chunk
+            inject: 'body', //預設<body> js </body>  head or body
+            template: './src/index.html',//來源
+            filename: 'index.html'
+            // 目的地
         })
-    ], // 對應的插件
+
+    ],  // 對應的插件
     // devServer: {},           // 服務器配置
     mode: 'development'      // 開發模式配置 production | development 
 }
